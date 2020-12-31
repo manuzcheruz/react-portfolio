aaef5fee0b0f5db0a6831f8eec9f5bde17e2f8b5
manuzcheruz <kipkemoi.emanuel@gmail.com>
2020-12-31 16:42:33 +0300

    added services and clients

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
 
+import './Clients.css'
+
+import libryo from '../../Assets/libryo.svg'
+import sakaajira from '../../Assets/sakaajira.svg'
+
 function Clients() {
     return (
         <div className="section" id="clients">
@@ -8,6 +13,12 @@ function Clients() {
             <div className="work-wrapper">
                 <Fade bottom>
                     <h1>Clients</h1>
+                </Fade>
+                <Fade bottom>
+                    <div className="svgs">
+                        <img class="filter-grey" alt="sakaajira" src={sakaajira} />
+                        <img class="filter-grey" alt="libryo" src={libryo} />
+                    </div>
                 </Fade>
             </div>
         </div>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
 
+import './Services.css'
+
 function Services() {
     return (
         <div className="section" id="clients">
         <div className="container">
-            <div className="work-wrapper">
+            <div className="services-wrapper">
                 <Fade bottom>
                     <h1>Services</h1>
+                </Fade>
+                <Fade bottom>
+                    <div className="row">
+                        <div className="column">
+                            <div className="card">
+                                <h2>
+                                    Strategy & Direction
+                                </h2>
+                                <h5>
+                                    Defining the problem, identifying the scope and finally, organising the design roadmap to bring out 100% of every project.
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <h2>
+                                    Design UI/UX
+                                </h2>
+                                <h5>
+                                    Websites, applications or other design related tasks. I love tackling digital problems and help fix them.
+                                </h5>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="card">
+                                <h2>
+                                    Mobile App
+                                </h2>
+                                <h5>
+                                    It's 2020, we should really step up our mobile game a bit more, right? Open to tackling new mobile projects.
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
                 </Fade>
             </div>
         </div>
