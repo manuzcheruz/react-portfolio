6e38f7e5b0e33844afa5bdb35bb2e5a2abff7aa6
manuzcheruz <kipkemoi.emanuel@gmail.com>
2021-01-05 18:44:07 +0300

    deploying to heroku

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Fade from 'react-reveal/Fade'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+import '../Resume/Resume.css'
 
 function Testimonial() {
+    const [ testimonials, setTestimonials ] = useState('')
+    useEffect(() => {
+      fetch('http://127.0.0.1:8000/testimonials/')
+        .then(res => {
+          return res.json()
+        })
+        .then(response => {
+          // console.log(response);
+          setTestimonials(response)
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }, [])
+
     return (
         <div className="section" id="clients">
         <div className="container">
@@ -9,6 +28,35 @@ function Testimonial() {
                 <Fade bottom>
                     <h1>Trusted by brands</h1>
                     <h1>all over the world</h1>
+                </Fade>
+                <Fade bottom>
+                    <CarouselProvider
+                        naturalSlideWidth={50}
+                        naturalSlideHeight={10}
+                        totalSlides={testimonials.length}
+                    >
+                        <Slider>
+                            {testimonials && testimonials.map((test, i) => {
+                                return (
+                                    <Slide index={i} key={i}>
+                                        <div className="row">
+                                            <div className="column">
+                                                <h5>hehe</h5>
+                                            </div>
+                                            <div className="column">
+                                                <h4>{test.title}</h4>
+                                                <h5>{test.description}</h5>
+                                                <h5>{test.name}</h5>
+                                                <h5>{test.role}</h5>
+                                            </div>
+                                        </div>
+                                    </Slide>
+                                )
+                            })}
+                        </Slider>
+                        <ButtonBack>Back</ButtonBack>
+                        <ButtonNext>Next</ButtonNext>
+                    </CarouselProvider>
                 </Fade>
             </div>
         </div>
